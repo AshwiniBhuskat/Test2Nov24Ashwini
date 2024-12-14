ae1ed521aeff02ac9ed69b74594e005713320f01
Ashwini Bhuskat <ashwini.bhuskat@gmail.com>
2024-12-14 12:05:11 +0530

    Second scenario conflict in branch B

--- src/ashwiniPrac/practice1.java
+++ src/ashwiniPrac/practice1.java
@@ -4,7 +4,7 @@ public class practice1 {
 	public static void main(String[] args) {
 		System.out.println("Hello Class1,Branch A Changes ");
 	    System.out.println("Hello Class1");
-		System.out.println("Hello Class1_Branch B Changes");
+		System.out.println("Hello Class1_Branch B Changes..Second Scenario conflict");
 	}
 }
 
