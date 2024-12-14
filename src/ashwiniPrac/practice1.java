eb473f44faeb6970406ca02fb91f977902ac8115
Ashwini Bhuskat <ashwini.bhuskat@gmail.com>
2024-12-14 12:18:53 +0530

    changes in A

--- src/ashwiniPrac/practice1.java
+++ src/ashwiniPrac/practice1.java
@@ -2,7 +2,7 @@ package ashwiniPrac;
 
 public class practice1 {
 	public static void main(String[] args) {
-		System.out.println("Hello Class1,Branch A Changes... ");
+		System.out.println("Hello Class1,Branch A Changes... Scenario 2 for A");
 	    System.out.println("Hello Class1..Branch A changes 2nd");
 		System.out.println("Hello Class1_Branch B Changes....CHnge in branch");
 	}
