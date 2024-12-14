72be995488b356629bf1a25f1fe0a1ceb510c2ee
Ashwini Bhuskat <ashwini.bhuskat@gmail.com>
2024-12-14 10:38:19 +0530

    prac1 and 2

--- src/ashwiniPrac/Practice2.java
+++ src/ashwiniPrac/Practice2.java
@@ -1,5 +1,10 @@
 package ashwiniPrac;
 
 public class Practice2 {
+	public static void main(String[] args) {
+		
+	
+System.out.println("Hello_class2");
 
+}
 }
--- src/ashwiniPrac/practice1.java
+++ src/ashwiniPrac/practice1.java
@@ -1,5 +1,8 @@
 package ashwiniPrac;
 
 public class practice1 {
-
+	public static void main(String[] args) {
+		System.out.println("Hello Class1");
+	}
 }
+
